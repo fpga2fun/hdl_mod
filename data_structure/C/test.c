b6c3b3a0f3962fa7b94b56dd82d15a7463acb122
fpga2fun <x18100696852@163.com>
2025-03-10 18:22:46 +0800

    重构 C 语言测试程序，更新指针和地址打印逻辑

--- data_structure/C/test.c
+++ data_structure/C/test.c
@@ -1,28 +1,12 @@
-#include<stdio.h> 
-
+#include <stdio.h> 
 int main(int argc, char *argv[]) 
 { 
-    unsigned int iNum1, iNum2, temp; 
+    int iNum = 0x64; 
+    int *ptr = &iNum; 
+    int **pPtr = &ptr; 
 
-    // 读取两个十六进制数
-    scanf("%x%x", &iNum1, &iNum2); 
-
-    // 打印两个数的地址
-    printf("%p, %p\n", &iNum1, &iNum2); 
-
-    // 打印两个数的值
-    printf("%x, %x\n", iNum1, iNum2); 
-
-    // 交换两个数的值
-    temp = iNum1; 
-    iNum1 = iNum2; 
-    iNum2 = temp; 
-
-    // 再次打印两个数的地址
-    printf("%p, %p\n", &iNum1, &iNum2); 
-
-    // 再次打印两个数的值
-    printf("%x, %x\n", iNum1, iNum2); 
-
+    printf("&iNum = 0x%p, iNum = 0x%x\n", (void*)&iNum, iNum); 
+    printf("&ptr = 0x%p, ptr = 0x%p, *ptr = 0x%x\n", (void*)&ptr, (void*)ptr, *ptr); 
+    printf("&pPtr = 0x%p, pPtr = 0x%p, *pPtr = 0x%p, **pPtr = 0x%x\n", (void*)&pPtr, (void*)pPtr, (void*)*pPtr, **pPtr); 
     return 0; 
 }
