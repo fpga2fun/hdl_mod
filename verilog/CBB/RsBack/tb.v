976428c4cf565f9f9fb98e2df4b05b408577c460
fpga2fun <x18100696852@163.com>
2025-05-15 18:09:15 +0800

    添加 CBB_RS_BACKWARD 模块及其测试基准，包含输入输出接口和状态逻辑

--- verilog/CBB/RsBack/tb.v
+++ verilog/CBB/RsBack/tb.v
@@ -24,6 +24,7 @@ module  tb();
 
     integer time_dly1,time_dly2;
 
+    integer sim_ctrl;
 
     initial begin
         i_rstn = 1'b0;
@@ -49,6 +50,7 @@ module  tb();
     end
 
     initial begin
+        sim_ctrl = 1;
         slv_i_valid = 1'b0;
         slv_i_data  = 0;
         @(posedge i_rstn);
@@ -66,40 +68,59 @@ module  tb();
             time_dly2 = $urandom_range(1,10);
             repeat(time_dly2) @(posedge i_clk);
         end
+        #200;
+        sim_ctrl = 0;
         #100;
         $finish;
     end
 
-    CBB_RS_FORWARD #(
-                       .P_DATA_WIDTH(P_DATA_WIDTH)
-                   ) U_CBB_RS_FORWARD (
-                       .i_clk       (i_clk),
-                       .i_rstn      (i_rstn),
-                       .slv_i_valid (slv_i_valid),
-                       .slv_i_data  (slv_i_data),
-                       .slv_o_ready (slv_o_ready),
-                       .mst_o_valid (mst_o_valid),
-                       .mst_o_data  (mst_o_data),
-                       .mst_i_ready (mst_i_ready)
-                   );
+    integer file_handle_slv,file_handle_mst;
+    initial begin
+        file_handle_slv = $fopen("./CBB_RS_BACKWARD_SLV_DATA.txt","w+");
+
+        while(sim_ctrl) begin
+            if(slv_i_valid&slv_o_ready) begin
+                $fdisplayh(file_handle_slv,slv_i_data);
+                $display("slv_i_data = %h",slv_i_data);
+                @(posedge i_clk);
+            end
+            else begin
+                @(posedge i_clk);
+            end
+        end
+        $fclose(file_handle_slv);
+    end
+
+    initial begin
+        file_handle_mst = $fopen("./CBB_RS_BACKWARD_MST_DATA.txt","w+");
+
+        while(sim_ctrl) begin
+            if(mst_o_valid&mst_i_ready) begin
+                $fdisplayh(file_handle_mst,mst_o_data);
+                @(posedge i_clk);
+            end
+            else begin
+                @(posedge i_clk);
+            end
+        end
+        $fclose(file_handle_mst);
+    end
+
+    CBB_RS_BACKWARD #(
+                        .P_DATA_WIDTH(P_DATA_WIDTH)
+                    ) U_CBB_RS_BACKWARD (
+                        .i_clk       (i_clk),
+                        .i_rstn      (i_rstn),
+                        .slv_i_valid (slv_i_valid),
+                        .slv_i_data  (slv_i_data),
+                        .slv_o_ready (slv_o_ready),
+                        .mst_o_valid (mst_o_valid),
+                        .mst_o_data  (mst_o_data),
+                        .mst_i_ready (mst_i_ready)
+                    );
     initial begin
         $dumpfile("wave.vcd");
         $dumpvars(0,tb);
         #5000 $finish;
     end
 endmodule
-/* @wavedrom
-{
-  "signal": [
-    {"name": "i_clk", "wave": "p.............."},
-    {"name": "i_rstn", "wave": "0.1......23..."},
-    {"name": "mst_i_ready", "wave": ".0.1..........."},
-    {"name": "slv_i_valid", "wave": ".0.1..........."},
-    {"name": "slv_i_data", "wave": ".0.1..........."},
-    {"name": "slv_o_ready", "wave": ".0.1..........."},
-    {"name": "mst_o_valid", "wave": ".0.1..........."},
-    {"name": "mst_o_data", "wave": ".0.1..........."}
-  ]
-}
-
-*/
