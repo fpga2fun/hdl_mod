441b67ae1aea7cc4059880a64147d76974dedfa7
fpga2fun <x18100696852@163.com>
2025-05-16 11:18:50 +0800

    优化 CBB_RS_BACKWARD 模块中的注释，增强代码可读性

--- verilog/CBB/RsBack/rsBack.v
+++ verilog/CBB/RsBack/rsBack.v
@@ -24,18 +24,18 @@ module  CBB_RS_BACKWARD#(
         if(~i_rstn) begin
             r_mst_i_ready <= 1'b1;
         end
-        else if (mst_o_valid) begin
+        else if (mst_o_valid) begin //前级valid有效时表示一定有数据传进来，或者该模块的ready无效时（即还没准备好把数据送走），所以此时需要把后级的ready寄存下来
             r_mst_i_ready <= mst_i_ready;
         end
     end
 
     reg 	[P_DATA_WIDTH-1:0] 	r_mst_o_data;//寄存后级ready的同时还需要把数据寄存下来
     always @(posedge i_clk) begin : proc_data
-        if (slv_i_valid & slv_o_ready) begin //当前级valid和寄存后的ready都有效时寄存来自前级的data
+        if (slv_i_valid & slv_o_ready) begin //当前级valid和寄存后的ready都有效时寄存（更新）来自前级的data，当后级反压前级时，寄存的data不变。
             r_mst_o_data <= slv_i_data;
         end
     end
-    assign mst_o_data = slv_o_ready ? slv_i_data  : r_mst_o_data;//寄存后的ready有效时输出来自前级的data，后级寄存的ready无效表示此时后级在反压前级，所以需要输出寄存后的data
+    assign mst_o_data = slv_o_ready ? slv_i_data  : r_mst_o_data;//寄存后的ready有效时（也就是该模块准备好时）直接输出来自前级的data，后级寄存的ready无效表示此时后级在反压前级（也就是该模块还没准备好），所以需要输出寄存后的data。
     assign slv_o_ready= r_mst_i_ready;
     assign mst_o_valid= slv_i_valid | (~slv_o_ready);
 
